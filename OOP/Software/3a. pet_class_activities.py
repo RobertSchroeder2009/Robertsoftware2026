2beaa4a0fb82c15baacfb0213617f6d6b9b19d02
RobertSchroeder2009 <Robert.schroeder55@outlook.com>
2026-05-10 23:06:16 +0000

    finished3a

--- OOP/Software/3a. pet_class_activities.py
+++ OOP/Software/3a. pet_class_activities.py
@@ -2,6 +2,28 @@
 # - Create a class pet with same information as in previous examples
 # - Create an object instance of class pet
 
+class Pet:
+    def __init__(self, name, category, age, vaccination, ccredit, baddress, oname, funds):
+        self.name = name
+        self.category = category
+        self.age = age
+        self.vaccination = False
+        self.ccredit = 'unknown'
+        self.baddress = 'unknown'
+        self.oname = 'unknown'
+        self.funds = 0
+
+p1 =  Pet('Bonnie', 'Cat', 3, True, '1847 3847 3904 8374', '37 south drive', 'John', 45.89)
+
+p2 = Pet('Foxy',  'Dog', 5, True, '3874 9347 3975 7340', '84 Jakes way', 'James', '26.54')
+
+print(p1.name)
+print(p1.category)
+print(p1.vaccination)
+
+print(p2.name)
+print(p2.category)
+print(p2.vaccination)
 
 
 
