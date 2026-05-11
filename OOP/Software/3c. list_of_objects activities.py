adf7f5cf1ed3011cf5de3e67bfadda9d01cbdd76
RobertSchroeder2009 <Robert.schroeder55@outlook.com>
2026-05-11 00:09:13 +0000

    nearly donce 3c

--- OOP/Software/3c. list_of_objects activities.py
+++ OOP/Software/3c. list_of_objects activities.py
@@ -10,8 +10,14 @@ class Pet:
         self.ccard = 'unknown'
         self.vaccinated = False
 
+    def __str__(self):
+        my_status = 'Names: ' + self.name + '\nCategory: ' + self.category + '\nAge: ' + str(self.age) + '\nCcredit: ' + self.ccard + '\nVaccination status: ' + str(self.vaccinated)
+        if 'unknown' in self.ccard:
+            print('no payment details registed')
+        return my_status
+    
 
-
+p1 = Pet('John pork', 'pig', 3, vaccinated = True)
 #ACTIVITIES:
 #1. Add another pet to the list (try different methods)
 #2. Vaccinate each pet in the list
